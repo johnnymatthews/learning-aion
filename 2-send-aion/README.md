70aedb896860411df2e0dcefaf7db4eebfe952d6
John <reelyard@protonmail.com>
2019-08-05 08:44:26 -0400

    Simplifying 2-send-aion.

--- 2-send-aion/README.md
+++ 2-send-aion/README.md
@@ -1,3 +1,3 @@
 # Send Aion
 
-This project is empty right now.
+In this project we'll create a simple web application where the user can send `AION` to another address. This is the 2nd project is a series, so if you haven't played around with the _Getter-Setter_ project, I suggest you take a look there first. It covers some topics that we're going to gloss over in this project.
--- 2-send-aion/js/script.js
+++ 2-send-aion/js/script.js
@@ -1,38 +1,19 @@
 const nodeUrl = "https://aion.api.nodesmith.io/v1/mastery/jsonrpc?apiKey=ab40c8f567874400a69c1e80a1399350";
-let contractAddress = "0xa003cd11951f9a58f81df851e83cf7b5eca4b2ca5d6429dadb49021c13603357";
 const web3 = new Web3(new Web3.providers.HttpProvider(nodeUrl));
 
 async function sendAion() {
-    let data = web3.avm.contract.method("getString").encode();
-    const transactionObject = {
-        to: contractAddress,
-        data: data,
-        type: "0x1"
-    };
-
-    let initialResponse = await web3.eth.call(transactionObject);
-    let avmResponse = await web3.avm.contract.decode("string", initialResponse);
-    document.querySelector('#current_string_output').innerHTML = avmResponse;
-}
-
-async function setString() {
     document.querySelector('#submit_button').innerHTML = 'Loading...';
     document.querySelector('#submit_button').disabled = true;
 
     let privateKeyInput = document.querySelector('#private_key_input').value;
-    let newStringInput = document.querySelector("#new_string_input").value;
+    let receivingAddressInput = document.querySelector("#receiving_address_input").value;
 
     const account = web3.eth.accounts.privateKeyToAccount(privateKeyInput);
 
-    let data = web3.avm.contract
-        .method("setString")
-        .inputs(["string"], [newStringInput])
-        .encode();
-
     const transaction = {
         from: account.address,
-        to: contractAddress,
-        data: data,
+        to: receivingAddressInput,
+        value: 1000000000000000000,
         gasPrice: 10000000000,
         gas: 2000000,
         type: "0x1"
@@ -52,13 +33,8 @@ async function setString() {
             );
         });
 
-    console.log("Transaction Receipt: ", transactionReceipt);
-    getString();
+    document.querySelector('#transaction_receipt_output').innerHTML = `Tranasction Receipt: <a target="_blank" href="https://mastery.aion.network/#/transaction/${transactionReceipt.transactionHash}">${transactionReceipt.transactionHash}</a>`
 
     document.querySelector('#submit_button').innerHTML = 'Submit';
     document.querySelector('#submit_button').disabled = false;
-}
-
-window.onload = function() {
-    getString();
 }
