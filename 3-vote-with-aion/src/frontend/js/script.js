66f3cd593977e837de1494da13dfde75157d2af3
Tanger71 <sawyercoletang@gmail.com>
2019-08-14 13:27:20 -0400

    added private key input and some other improvments

--- 3-vote-with-aion/src/frontend/js/script.js
+++ 3-vote-with-aion/src/frontend/js/script.js
@@ -1,37 +1,9 @@
 const nodeUrl = "https://aion.api.nodesmith.io/v1/mastery/jsonrpc?apiKey=da85417fac594f0099708ad6e7ea2e97";
 const web3 = new Web3(new Web3.providers.HttpProvider(nodeUrl));
-const pk = "d2abbfb69e2927abea2388ce74aa3199d86293a4704de0c6ff3572ec6719fefa83f31c24396498eccd1f4eaf690f9e24110c5a1c55ef436c1fe1aea5b452af40";
+let pk='';
 
 let contractAddress = "0xa0c468ba67c12a47637fb21c786b870085b81d6182be2fb2812ac7616dfc8672";
-let abi = `
-0.0
-VotingWithAion.Voting
-Clinit: ()
-public static String getQuestion(int)
-public static boolean getQuestionStatus(int)
-public static int getRequiredVotes(int)
-public static String[] getChoices(int)
-public static String[] getVotes(int)
-public static int getNumberQuestions()
-public static void newQuestion(String, String[], int)
-public static void newVote(int, String)
-`;
 
-abi = `
-0.0
-VotingWithAion.Voting
-Clinit: ()
-public static String getQuestion(int)
-public static boolean getQuestionStatus(int)
-public static int getRequiredVotes(int)
-public static String[] getChoices(int)
-public static String[] getVotes(int)
-public static int getNumberQuestions()
-public static void newVote(int, String)
-`;
-
-let abiObj = web3.avm.contract.Interface(abi);
-web3.avm.contract.initBinding(contractAddress, abiObj, pk);
 
 class Poll {
     constructor(question, status, requiredVotes, choices, votes){
@@ -53,24 +25,87 @@ async function getPollObject(questionID){
     );
 }
 
-function makeButton(questionID, choice){
-    return `<button style="margin:5px;" type='button' id='vote_button' onclick='newVote(${questionID}, "${choice}")'>${choice}</button>`;
+async function enterPrivateKey(){
+    pk = document.querySelector('#private_key_input').value;
+
+    let abi = `
+    0.0
+    VotingWithAion.Voting
+    Clinit: ()
+    public static String getQuestion(int)
+    public static boolean getQuestionStatus(int)
+    public static int getRequiredVotes(int)
+    public static String[] getChoices(int)
+    public static String[] getVotes(int)
+    public static int getNumberQuestions()
+    public static void newQuestion(String, String[], int)
+    public static void newVote(int, String)
+`;
+
+    abi = `
+    0.0
+    VotingWithAion.Voting
+    Clinit: ()
+    public static String getQuestion(int)
+    public static boolean getQuestionStatus(int)
+    public static int getRequiredVotes(int)
+    public static String[] getChoices(int)
+    public static String[] getVotes(int)
+    public static int getNumberQuestions()
+    public static void newVote(int, String)
+`;
+
+    let abiObj = web3.avm.contract.Interface(abi);
+    web3.avm.contract.initBinding(contractAddress, abiObj, pk);
+    await drawPollPicker(await getNumberQuestions());
 }
 
-async function getPoll(){
+async function drawPollPicker(numberQuestions){
+
+    let html = `
+        <hr>
+        <text style="padding-right:10px;">Enter the questionID you would like to vote on:</text>
+        <select id="poll_picker" style="width:70px;display:inline-block;margin-right: 10px" name="questionIDs">
+    `;
+
+    for(let i = 0 ; i<numberQuestions ; i++)
+        html += `<option value=${i}>${i}</option>`;
+
+    html += `</select><button type="button" id="getPoll_button" onclick="drawPoll()">Get Poll</button>`;
+
+    document.getElementById("poll_picker_div").innerHTML += html;
+}
+
+function makePollButton(questionID, choice, numberVotes){
+    return `<button style="margin:5px;" type='button' id='vote_button' onclick='newVote(${questionID}, "${choice}")'>${choice} (${numberVotes})</button>`;
+}
+
+async function drawPoll(){
     let questionID = document.querySelector('#poll_picker').value;
+
     let poll = await getPollObject(questionID);
 
-    document.getElementById("poll_buttons").innerHTML = '';
+    let html = `
+        <hr>
+        <h4 style="margin-bottom:5px;">Question: ${poll.question}</h4>
+        <p style="margin-bottom:5px;">Each account may vote once. A second vote will cause a failed transaction.</p>
+        <div id="poll_buttons">
+    `;
 
-    let votes = document.createElement("div");
 
-    for (let i in poll.choices) {
-        votes.innerHTML += makeButton(questionID, poll.choices[i])
+    let voteCount = 0;
+    for (let i in poll.choices){
+        for(let j in poll.votes)
+            if(poll.votes[j] === poll.choices[i])
+                voteCount++;
+        html += makePollButton(questionID, poll.choices[i], voteCount);
+        voteCount=0;
     }
-    document.getElementById("poll_buttons").appendChild(votes);
+    html += `</div>`;
 
-    document.querySelector('#question_name').innerHTML = `Question: ${poll.question}`
+    document.getElementById("poll_div").innerHTML = html;
+    document.querySelector('#transaction_receipt_output').innerHTML = ``;
+
 }
 
 async function getQuestion(questionID) {
@@ -98,11 +133,15 @@ async function getNumberQuestions() {
 }
 
 async function newVote(questionID, choice) {
-    // document.querySelector('#submit_button').innerHTML = 'Loading...';
-    // document.querySelector('#submit_button').disabled = true;
+    let c = document.getElementById("poll_buttons").childNodes;
+    console.log(document.getElementById("poll_buttons"));
+    for (i = 0; i < c.length; i++) {
+        c[i].disabled = true;
+    }
 
-    // let privateKeyInput = document.querySelector('#private_key_input').value;
-    let privateKeyInput = pk;
+    document.querySelector('#transaction_receipt_output').innerHTML = `Awaiting Transaction...`;
+
+    let privateKeyInput = document.querySelector('#private_key_input').value;
     const account = web3.eth.accounts.privateKeyToAccount(privateKeyInput);
 
     let data = web3.avm.contract
@@ -134,11 +173,11 @@ async function newVote(questionID, choice) {
         });
 
     console.log("Transaction Receipt: ", transactionReceipt);
+    document.querySelector('#transaction_receipt_output').innerHTML = `Tranasction Receipt: <a target="_blank" href="https://mastery.aion.network/#/transaction/${transactionReceipt.transactionHash}">${transactionReceipt.transactionHash}</a>`;
 
-    // document.querySelector('#submit_button').innerHTML = 'Submit';
-    // document.querySelector('#submit_button').disabled = false;
+    drawPoll();
 }
 
-window.onload = function() {
-    // getQuestion(0);
+window.onload = async function() {
+
 };
